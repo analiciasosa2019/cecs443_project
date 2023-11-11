75a22b62ee07ff6993552c5dad1291f408b64045
analiciasosa2019 <115589709+analiciasosa2019@users.noreply.github.com>
2023-11-11 14:43:32 -0800

    added css to project

--- parking_manager/views.py
+++ parking_manager/views.py
@@ -5,23 +5,23 @@ def index(request):
     user = "John"  # Replace this with your user data
     return render(request, 'parking_manager/index.html', {'user': user})
 
-def payment(request):
-    return render(request, 'parking_manager/pages/payment.html')
+def payment():
+    return
 
-def res_cancel_confirmation(request):
-    return render(request, 'parking_manager/pages/res_cancel_confirmation.html')
+def res_cancel_confirmation():
+    return
 
-def res_cancellation(request):
-    return render(request, 'parking_manager/pages/res_cancellation.html')
- 
-def res_confirmation(request):
-    return render(request, 'parking_manager/pages/res_confirmation.html')
+def res_cancellation():
+    return
 
-def res_mod_confirmation(request):
-    return render(request, 'parking_manager/pages/res_mod_confirmation.html')
+def res_confirmation():
+    return
 
-def res_modification(request):
-    return render(request, 'parking_manager/pages/res_modification.html')
+def res_mod_confirmation():
+    return
+
+def res_modification():
+    return
 
 def reservation(request):
-    return render(request, 'parking_manager/pages/reservation.html')
+    return
