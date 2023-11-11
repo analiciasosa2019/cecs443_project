c996f6f12036b47edf4b6cf7604a16f53c0c90bb
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-11-11 14:51:59 -0800

    edited urls

--- parking_manager/views.py
+++ parking_manager/views.py
@@ -2,26 +2,25 @@ from django.shortcuts import render
 
 
 def index(request):
-    user = "John"  # Replace this with your user data
-    return render(request, 'parking_manager/index.html', {'user': user})
+    return render(request, 'parking_manager/index.html')
 
-def payment():
-    return
+def payment(request):
+    return render(request, 'parking_manager/pages/payment.html')
 
-def res_cancel_confirmation():
-    return
+def res_cancel_confirmation(request):
+    return render(request, 'parking_manager/pages/res_cancel_confirmation.html')
 
-def res_cancellation():
-    return
+def res_cancellation(request):
+    return render(request, 'parking_manager/pages/res_cancellation.html')
 
-def res_confirmation():
-    return
+def res_confirmation(request):
+    return render(request, 'parking_manager/pages/res_confirmation.html')
 
-def res_mod_confirmation():
-    return
+def res_mod_confirmation(request):
+    return render(request, 'parking_manager/pages/res_mod_confirmation.html')
 
-def res_modification():
-    return
+def res_modification(request):
+    return render(request, 'parking_manager/pages/res_modification.html')
 
 def reservation(request):
-    return
+    return render(request, 'parking_manager/pages/reservation.html')
