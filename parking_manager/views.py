9f47cdf1f93680a91c0db2c34117c57ad64efd2c
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-11-11 13:43:59 -0800

    created all pages

--- parking_manager/views.py
+++ parking_manager/views.py
@@ -5,23 +5,23 @@ def index(request):
     user = "John"  # Replace this with your user data
     return render(request, 'parking_manager/index.html', {'user': user})
 
-def payment():
-    return
+def payment(request):
+    return render(request, 'parking_manager/payment.html')
 
-def res_cancel_confirmation():
-    return
+def res_cancel_confirmation(request):
+    return render(request, 'parking_manager/res_cancel_confirmation.html')
 
-def res_cancellation():
-    return
+def res_cancellation(request):
+    return render(request, 'parking_manager/res_cancellation.html')
+ 
+def res_confirmation(request):
+    return render(request, 'parking_manager/res_confirmation.html')
 
-def res_confirmation():
-    return
+def res_mod_confirmation(request):
+    return render(request, 'parking_manager/res_mod_confirmation.html')
 
-def res_mod_confirmation():
-    return
+def res_modification(request):
+    return render(request, 'parking_manager/res_modification.html')
 
-def res_modification():
-    return
-
-def reservation():
-    return
+def reservation(request):
+    return render(request, 'parking_manager/reservation.html')
