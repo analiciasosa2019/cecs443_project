0288d13945e859476bbd30999904ce7bf6cf243b
ralfbrse <raphaeloburce@gmail.com>
2023-11-30 01:03:35 -0800

    Fixed max spots, taken spots on day, cancellations

--- parking_manager/urls.py
+++ parking_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from parking_manager import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("payment", views.payment, name="payment"),
     path("res_cancel_confirmation", views.res_cancel_confirmation, name="res_cancel_confirmation"),
     path("res_cancellation", views.res_cancellation, name="res_cancellation"),
@@ -12,6 +12,7 @@ urlpatterns = [
     path("res_modification", views.res_modification, name="res_modification"),
     path("reservation", views.reservation, name="reservation"),
     path("res_modification_detail", views.res_modification_detail, name="res_modification_detail"),
+   
 
 
 ]
--- parking_manager/views.py
+++ parking_manager/views.py
@@ -5,6 +5,7 @@ import ssl
 import smtplib
 import requests as req
 import json
+import random
 
 config ={
     "apiKey": "AIzaSyB7x88N6q8Mq4glPuJk15xoGuiV1d4EPWQ",
@@ -19,7 +20,7 @@ config ={
 firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database = firebase.database()
-
+max_spots = 15
 def index(request):
     return render(request, 'parking_manager/index.html')
 
@@ -36,10 +37,17 @@ def res_cancel_confirmation(request):
 def res_cancellation(request):
     if request.method == 'POST':
         code = request.POST['Code']
+        pin = request.POST['PIN']
+     
         if not database.child(code).shallow().get().val() or code == "":
             print("users does not exist")
         else:
             print("users exist")
+            pin_in_db = str(database.child(code).child('Pin Number').get().val())
+            print(pin_in_db, type(pin_in_db))
+            print(pin, type(pin))
+            if pin_in_db != pin:
+                return render(request, 'parking_manager/pages/res_modification_rejection.html')
             database.child(code).remove()
             return render(request, 'parking_manager/pages/res_cancel_confirmation.html')
     return render(request, 'parking_manager/pages/res_cancellation.html')
@@ -53,10 +61,17 @@ def res_mod_confirmation(request):
 def res_modification(request):
     if request.method == 'POST':
         code = request.POST['Code']
+        pin = request.POST['PIN']
+     
         if not database.child(code).shallow().get().val() or code == "":
             print("users does not exist")
         else:
             print("users exist")
+            pin_in_db = str(database.child(code).child('Pin Number').get().val())
+            print(pin_in_db, type(pin_in_db))
+            print(pin, type(pin))
+            if pin_in_db != pin:
+                return render(request, 'parking_manager/pages/res_modification_rejection.html')
             database.child(code).remove()
             return render(request, 'parking_manager/pages/res_modification_detail.html')
     return render(request, 'parking_manager/pages/res_modification.html')
@@ -64,9 +79,10 @@ def res_modification(request):
 def reservation(request):
     x = database.child().get().val()
     dates = list(x.values())
-    print(dates)
+    current_spots = len(dates)
+    list_dates = []
     for i in dates:
-        print(i['Date'])
+        list_dates.append(i['Date'])
     if request.method == 'POST':
         #print(request.POST['First_Name'])
         ln = request.POST['Last_Name']
@@ -85,65 +101,103 @@ def reservation(request):
         
         date = str(month) + "/" + str(day) + "/" + str(year)
         cdate = str(cmonth) + "/" + str(cday) + "/" + str(cyear)
-        h = hash(date + str(pn)) 
-        ch = str(h)
+        # h = hash(date + str(pn)) 
+        ch = str(month) +str(day)  + str(year) + str(pn)
 
         valiDATE(date)
 
         if not database.child(ch).shallow().get().val():
+            parking_pin = random.randint(1000,9999)
             pay = {'Card_Num': card_num, 'EXP': cdate}
             data = {'First_Name': fn , 'Last_Name': ln, 'Date': date, 'Email' : email, 'License' : li, 'Payment' : "",
-                    'Park_Num': pn, 'Payment': pay}
+                    'Park_Num': pn, 'Pin Number': parking_pin, 'Payment': pay}
+            beef = 0
+            
+            for i in list_dates:
+                if i == date:
+                    beef+= 1
+                    print(beef)
+            if beef >= max_spots -1:
+                return render(request, 'parking_manager/pages/res_rejection_lotful.html')
+                
+            
+            
             database.child(ch).set(data)
             print("hmm")
-            sendConf(email, ch)
+            
+            sendConf(email, ch, parking_pin)
             return render(request, 'parking_manager/pages/res_confirmation.html')
         else:
             print("Already taken")
-            return render(request, 'parking_manager/pages/reservation.html')
+            return render(request, 'parking_manager/pages/res_rejection_spottaken.html')
     return render(request, 'parking_manager/pages/reservation.html')
+
+def is_spot_taken(date, spot):
+    return
 
 def res_modification_detail(request):
     if request.method == 'POST':
-        # print(request.POST['First_Name'])
-        ln = request.POST['Last_Name']
-        email = request.POST['Email']
-        pn = request.POST['Park_Num']
-        li = request.POST['License']
-        fn = request.POST['First_Name']
-        year = request.POST['Year']
-        month = request.POST['Month']
-        day = request.POST['Day']
+        x = database.child().get().val()
+        dates = list(x.values())
+        current_spots = len(dates)
+        list_dates = []
+        for i in dates:
+            list_dates.append(i['Date'])
+        if request.method == 'POST':
+            #print(request.POST['First_Name'])
+            ln = request.POST['Last_Name']
+            email = request.POST['Email']
+            pn = request.POST['Park_Num']
+            li = request.POST['License']
+            fn = request.POST['First_Name']
+            year = request.POST['Year']
+            month = request.POST['Month']
+            day = request.POST['Day']
 
-        card_num = request.POST['Card_Num']
-        cyear = request.POST['Card_Y']
-        cmonth = request.POST['Card_M']
-        cday = request.POST['Card_D']
-        
-        date = str(month) + "/" + str(day) + "/" + str(year)
-        cdate = str(cmonth) + "/" + str(cday) + "/" + str(cyear)
-        h = hash(date + str(pn)) 
-        ch = str(h)
+            card_num = request.POST['Card_Num']
+            cyear = request.POST['Card_Y']
+            cmonth = request.POST['Card_M']
+            cday = request.POST['Card_D']
+            
+            date = str(month) + "/" + str(day) + "/" + str(year)
+            cdate = str(cmonth) + "/" + str(cday) + "/" + str(cyear)
+            # h = hash(date + str(pn)) 
+            ch = str(month) +str(day)  + str(year) + str(pn)
+
+        valiDATE(date)
+
         if not database.child(ch).shallow().get().val():
+            parking_pin = random.randint(1000,9999)
             pay = {'Card_Num': card_num, 'EXP': cdate}
             data = {'First_Name': fn , 'Last_Name': ln, 'Date': date, 'Email' : email, 'License' : li, 'Payment' : "",
-                    'Park_Num': pn, 'Payment': pay}
+                    'Park_Num': pn, 'Pin Number': parking_pin, 'Payment': pay}
+            beef = 0
+            
+            for i in list_dates:
+                if i == date:
+                    beef+= 1
+                    print(beef)
+            if beef >= max_spots -1:
+                return render(request, 'parking_manager/pages/res_rejection_lotful.html')
+                
+            
+            
             database.child(ch).set(data)
-            print(database.child(ch).shallow().get().val())
             print("hmm")
-            #sendConf(email, ch)
+            
+            sendConf(email, ch, parking_pin)
             return render(request, 'parking_manager/pages/res_mod_confirmation.html')
         else:
             print("Already Taken")
             return render(request, 'parking_manager/pages/res_modification_detail.html')
     return render(request, 'parking_manager/pages/res_modification_detail.html')
 
-def sendConf(email, hsh):
+def sendConf(email, hsh, pin):
         sender = "hankbank891@gmail.com"
         password = "tpdb eteh qcdx spep"
         receiver = email
         subject = "Your Reservation is Complete"
-        body = 'confirmation number: ' + hsh
+        body = 'confirmation number: ' + hsh + '\npin:' + str(pin)
         em = EmailMessage()
         em['From'] = sender
         em['To'] = receiver
